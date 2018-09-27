67418e376ed90e6ab85898e0cbe4e633816d9c85
DadMuscles <zcutshall@gmail.com>
2018-09-27 11:39:34 -0500

    added a base script for managing player status and effects. Next up is implementing basic networking for a sweet airborn laser duel.

---
+++ Assets/Scripts/PlayerStatusController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStatusController : MonoBehaviour {
+
+    //Collider is probably not needed
+    public CapsuleCollider2D playerCollider;
+
+    public float maxHealth = 150;
+    public float health;
+    public float maxArmor = 0;
+    public float armor;
+    public float maxShield = 0;
+    public float shield = 0;
+
+    private bool dead = false;
+
+	// Use this for initialization
+	void Start () {
+        health = maxHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (health <= 0)
+        {
+            dead = true;
+        }
+
+        if (dead)
+        {
+            Debug.Log("Died");
+        }
+	}
+
+    public void adjustHealth(float hpDif)
+    {
+        if(health + hpDif <= maxHealth)
+        {
+            health += hpDif;
+        }
+        
+    }
+
+}
